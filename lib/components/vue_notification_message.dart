9ca4d955df2965efc3556dc280fd6b935a30490e
Sébastien Gratade <sebastien.gratade@etu.univ-orleans.fr>
2024-03-24 16:49:08 +0100

    fin front notifications

--- lib/components/vue_notification_message.dart
+++ lib/components/vue_notification_message.dart
@@ -1,17 +1,131 @@
+import 'package:allo/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class VueNotificationMessage extends StatelessWidget{
-  String imagePath;
+class VueNotificationMessage extends StatelessWidget {
+  String? imagePath;
   String pseudo;
   String message;
   String date;
   int nbNotifs;
 
-  VueNotificationMessage({required this.imagePath, required this.pseudo, required this.message, required this.date, required this.nbNotifs});
+  VueNotificationMessage(
+      {this.imagePath,
+      required this.pseudo,
+      required this.message,
+      required this.date,
+      required this.nbNotifs});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Row(
+          children: [
+            if (imagePath == null)
+              ClipOval(
+                child: Container(
+                  alignment: Alignment.center,
+                  color: AppColors.lightBlue,
+                  width: 60, // you can adjust width and height to your liking
+                  height:
+                      60, // you can adjust width and height to your liking
+                  child: Text(
+                    pseudo[0].toUpperCase(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      fontFamily: "NeueRegrade",
+                    ),
+                  ),
+                ),
+              ),
+            if (imagePath != null)
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(imagePath!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            SizedBox(width: 13),
+            Expanded(
+                child: Stack(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      pseudo,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.dark,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: "NeueRegrade",
+                      ),
+                    ),
+                    LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Container(
+                          width: constraints.maxWidth *
+                              0.85, // 70% de la largeur du conteneur
+                          child: Text(
+                            message,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.darkTertiary,
+                                fontFamily: "NeueRegrade",
+                                fontWeight: FontWeight.w400),
+                          ),
+                        );
+                      },
+                    ),
+                    Text(
+                      date,
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: AppColors.darkSecondary,
+                          fontFamily: "NeueRegrade",
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    width: 25,
+                    height: 25,
+                    decoration: BoxDecoration(
+                      color: AppColors.notification,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        nbNotifs.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "NeueRegrade",
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
   }
 }
--- lib/widgets/page_notifications.dart
+++ lib/widgets/page_notifications.dart
@@ -1,10 +1,14 @@
 import 'package:allo/components/ListeAnnonce.dart';
 import 'package:allo/components/custom_text_field.dart';
 import 'package:allo/components/top_selection_menu.dart';
+import 'package:allo/components/vue_notification.dart';
+import 'package:allo/components/vue_notification_message.dart';
 import 'package:allo/constants/app_colors.dart';
 import 'package:allo/models/annonce.dart';
 import 'package:allo/models/app_bar_title.dart';
 import 'package:allo/models/index_page_notifications.dart';
+import 'package:allo/models/notification.dart';
+import 'package:allo/models/notification_message.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,8 +30,6 @@ class _HomeState extends State<PageNotifications> {
       _pageController.jumpToPage(
           Provider.of<IndexPageNotifications>(context, listen: false).index);
     });
-
-
   }
 
   @override
@@ -35,6 +37,57 @@ class _HomeState extends State<PageNotifications> {
     // TODO: implement dispose
     super.dispose();
   }
+
+  // les deux ont comme image assets/perceuse.jpeg
+  // les deux ont comme titre d'annonce: Besoin d'une perceuse le 24/02/2024
+  // les deux ont comme date: il y a 2 heures
+  // un a comme intitulé: Vous avez reçu une demande de location
+  // l'autre a comme intitulé: a enregistré votre annonce 
+  List<NotificationAnnonce> notifsAnnonce = [
+    NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "Vous avez reçu une demande de location"),
+    NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "a enregistré votre annonce"),
+     NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "Vous avez reçu une demande de location"),
+    NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "a enregistré votre annonce"),
+     NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "Vous avez reçu une demande de location"),
+    NotificationAnnonce(
+        imagePath: "assets/perceuse.jpeg",
+        titreAnnonce: "Besoin d'une perceuse le 24/02/2024",
+        date: "il y a 2 heures",
+        intitule: "a enregistré votre annonce"),
+  ];
+
+  List<NotificationMessage> notifsMessage = [
+    NotificationMessage(
+        pseudo: "Jean",
+        message: "Salut, je suis intéressé par votre annonce",
+        date: "il y a 2 heures",
+        nbNotifs: 2),
+    NotificationMessage(
+        pseudo: "Theo Pavillon",
+        message: "Salut, je suis intéressé par votre annonce",
+        date: "il y a 2 heures",
+        nbNotifs: 2),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +104,7 @@ class _HomeState extends State<PageNotifications> {
             );
           },
         )),
+        SizedBox(height: 24,),
         Expanded(
           // Ajoutez ce widget
           child: PageView(
@@ -62,17 +116,44 @@ class _HomeState extends State<PageNotifications> {
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Container(
-                  height: 500,
-                  color: AppColors.lightBlue,
+                child: CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          return VueNotification(
+                            imagePath: notifsAnnonce[index].imagePath,
+                            titreAnnonce: notifsAnnonce[index].titreAnnonce,
+                            date: notifsAnnonce[index].date,
+                            intitule: notifsAnnonce[index].intitule,
+                          );
+                        },
+                        childCount: notifsAnnonce.length,
+                      ),
+                    ),
+                  ],
                 ),
               ),
                Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Container(
-                  height: 500,
-                  color: AppColors.danger,
-                ),
+                child: CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          return VueNotificationMessage(
+                            imagePath: notifsMessage[index].imagePath,
+                            pseudo: notifsMessage[index].pseudo,
+                            message: notifsMessage[index].message,
+                            date: notifsMessage[index].date,
+                            nbNotifs: notifsMessage[index].nbNotifs,
+                          );
+                        },
+                        childCount: notifsMessage.length,
+                      ),
+                    ),
+                  ],
+                )
               ),
             ],
           ),
