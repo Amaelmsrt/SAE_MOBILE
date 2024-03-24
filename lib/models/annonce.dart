cc4269c5cac79375e4f35560902d0265ee2c6b78
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2024-03-24 14:36:53 +0100

    modification du modèle d'une annonce

--- lib/models/annonce.dart
+++ lib/models/annonce.dart
@@ -1,15 +1,47 @@
 class Annonce {
-  String imageLink;
-  int niveauUrgence;
-  String titre;
-  bool isSaved;
-  double prix;
+  int idAnnonce;
+  String titreAnnonce;
+  String descriptionAnnonce;
+  DateTime datePubliAnnonce;
+  DateTime dateAideAnnonce;
+  bool estUrgente;
+  int etatAnnonce;
+  int idUtilisateur;
 
   Annonce({
-    required this.imageLink,
-    required this.niveauUrgence,
-    required this.titre,
-    required this.isSaved,
-    required this.prix,
+    required this.idAnnonce,
+    required this.titreAnnonce,
+    required this.descriptionAnnonce,
+    required this.datePubliAnnonce,
+    required this.dateAideAnnonce,
+    required this.estUrgente,
+    required this.etatAnnonce,
+    required this.idUtilisateur,
   });
+
+  factory Annonce.fromJson(Map<String, dynamic> json) {
+    return Annonce(
+      idAnnonce: json['idAnnonce'],
+      titreAnnonce: json['titreAnnonce'],
+      descriptionAnnonce: json['descriptionAnnonce'],
+      datePubliAnnonce: DateTime.parse(json['datePubliAnnonce']),
+      dateAideAnnonce: DateTime.parse(json['dateAideAnnonce']),
+      estUrgente: json['estUrgente'],
+      etatAnnonce: json['etatAnnonce'],
+      idUtilisateur: json['idUtilisateur'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'idAnnonce': idAnnonce,
+      'titreAnnonce': titreAnnonce,
+      'descriptionAnnonce': descriptionAnnonce,
+      'datePubliAnnonce': datePubliAnnonce.toIso8601String(),
+      'dateAideAnnonce': dateAideAnnonce.toIso8601String(),
+      'estUrgente': estUrgente,
+      'etatAnnonce': etatAnnonce,
+      'idUtilisateur': idUtilisateur,
+    };
+  }
 }
