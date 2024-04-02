8ac0845223955990b2dbea8af78f0ce352ac71df
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2024-04-02 09:09:25 +0200

    la barre de recherche fonctionne dans la partie accueil

--- lib/widgets/accueil.dart
+++ lib/widgets/accueil.dart
@@ -15,6 +15,33 @@ class _HomeState extends State<Accueil> {
   late Future<List<Annonce>> toutesLesAnnonces = AnnonceDB.fetchAllAnnonces();
   late Future<List<Annonce>> dernieresAnnonces = AnnonceDB.fetchLastAnnonces();
   late Future<List<Annonce>> annoncesUrgentes = AnnonceDB.fetchUrgentAnnonces();
+  final TextEditingController _searchController = TextEditingController();
+  Future<List<Annonce>>? _searchResults;
+
+  void searchListener() {
+    print("nouveau texte: " + _searchController.text);
+    if (_searchController.text.isEmpty) {
+      setState(() {
+        _searchResults = null;
+      });
+    } else {
+      AnnonceDB.findAnnonces(_searchController.text)
+          .then((List<String> value) {
+        if (!mounted) {
+          print("was not mounted");
+          return;
+        }
+        if (_searchController.text.isEmpty) {
+          return;
+        }
+        setState(() {
+          _searchResults = Future.wait(value.map((title) {
+            return AnnonceDB.fetchAnnonceByTitle(title);
+          }));
+        });
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -22,54 +49,76 @@ class _HomeState extends State<Accueil> {
     Future.delayed(Duration.zero, () {
       Provider.of<AppBarTitle>(context, listen: false).setTitle('Accueil');
     });
+
+    _searchController.addListener(searchListener);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(searchListener);
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return (Padding(
-        padding: new EdgeInsets.fromLTRB(15, 0, 15, 0),
-        child: ListView(
-          children: <Widget>[
-            Padding(
-                padding: new EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: CustomTextField(
-                    hint: "Rechercher une annonce...",
-                    iconPath: "assets/icons/loupe.svg")),
+    return Padding(
+      padding: new EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: new EdgeInsets.fromLTRB(0, 0, 0, 0),
+            child: CustomTextField(
+                controller: _searchController,
+                hint: "Rechercher une annonce...",
+                iconPath: "assets/icons/loupe.svg"),
+          ),
+          _searchResults != null
+              ? FutureBuilder(
+                  future: _searchResults,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Text('Erreur: ${snapshot.error}');
+                    } else {
+                      return ListeAnnonce(
+                          titre: _searchController.text.isEmpty
+                              ? "Vous pouvez les aider !"
+                              : "Résultats de la recherche",
+                          annonces: snapshot.data!,
+                          isVertical: true);
+                    }
+                  },
+                )
+              : Column(
+                  children: <Widget>[
+                    buildAnnonceList(
+                        toutesLesAnnonces, "Vous pouvez les aider !"),
+                    SizedBox(height: 24),
+                    buildAnnonceList(
+                        dernieresAnnonces, "Vous pouvez les aider !"),
+                    SizedBox(height: 24),
+                    buildAnnonceList(
+                        annoncesUrgentes, "Vous pouvez les aider !"),
+                    SizedBox(height: 24),
+                  ],
+                ),
+        ],
+      ),
+    );
+  }
 
-            FutureBuilder(future: toutesLesAnnonces, builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return ListeAnnonce(
-                    titre: "Vous pouvez les aider !", annonces: snapshot.data!);
-              }
-            }),
-            SizedBox(
-              height: 24,
-            ),
-            FutureBuilder(future: dernieresAnnonces, builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return ListeAnnonce(
-                    titre: "Vous pouvez les aider !", annonces: snapshot.data!);
-              }
-            }),
-            SizedBox(
-              height: 24,
-            ),
-            FutureBuilder(future: annoncesUrgentes, builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return ListeAnnonce(
-                    titre: "Vous pouvez les aider !", annonces: snapshot.data!);
-              }
-            }),
-            SizedBox(
-              height: 24,
-            ),
-          ],
-        )));
+  Widget buildAnnonceList(Future<List<Annonce>> annoncesFuture, String titre) {
+    return FutureBuilder(
+      future: annoncesFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          return ListeAnnonce(titre: titre, annonces: snapshot.data!);
+        }
+      },
+    );
   }
 }
